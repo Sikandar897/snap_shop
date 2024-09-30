bb8a800e3da9e3258086b2c1bd6f20a1cf9d8d19
Sikandar897 <sikandarafridi6@gmail.com>
2024-09-30 12:49:30 +0500

    fix: app

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Teleyork',
+      title: 'SnapShop',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
